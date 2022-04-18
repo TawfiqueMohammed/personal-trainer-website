16c74822bd59c84d44be542d84c2de46def0ebb4
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-04-18 12:41:11 +0600

    google signin done

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import google from '../../../images/Google-logo-design-clipart-PNG.png'
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    let errorElement;
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div>
-            <h2>Social Login</h2>
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className='bg-warning w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className='bg-warning w-50'></div>
+            </div>
+            {errorElement}
+            <div className=''>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-warning w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={google} alt="" />
+                    <span className='px-2'>Continue With Google</span>
+                </button>
+            </div>
         </div>
     );
 };
