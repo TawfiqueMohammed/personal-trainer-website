2299e9efc2583abbced28ead39ba0977c4ebddc0
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-04-18 13:12:59 +0600

    about me done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className='container'>
+            <h2 className='mt-3 text-center fw-bold'>About Me</h2>
+            <h4 className='mt-3 text-center'>Name: </h4>
+            <p className='text-center'>Tawfique Mohammed Tarek</p>
+            <h5 className='mt-3 text-center'>My Goal: </h5>
+            <p className='text-center'>My aim in life has always been to become a successful software engineer. Software engineering is highly respected in the field of information technology as almost every field is associated with computers these days.
+                The role of a software engineer becomes crucial in tapping the available sources to provide the best solution for issues faced.
+                Checking out the latest software upgrades has to be considered as well. Software engineering will help me in realizing my dream of making it big in the severe competition involved in the corporate market.</p>
+            <h5 className='mt-3 text-center'>My working procedure to achieve my goal:</h5>
+            <p className='text-center'>I will work hard and give my 100% to achieve my goal. Online is where I have the opportunity to share what I am learning and working on. It will also help me express what I am interested in, my mission for becoming a software developer, and who I am. So, with making proper routine I will try my best to achieve my goal.</p>
+
         </div>
     );
 };
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div className='container'>
-            <h2 className='text-center pt-3 text-warning fw-bold'>Blogs</h2>
+            <h2 className='text-center pt-3 fw-bold'>Blogs</h2>
             <h4><span className='fw-bold'>Question:</span> Differences Between Authorization and Authentication</h4>
             <p><span className='fw-bold'>Answer:</span> Authentication is the process of verifying who a user is, while authorization is the process of verifying what they have access to. Comparing these processes to a real-world example, when you go through security in an airport, you show your ID to authenticate your identity. </p>
             <h4><span className='fw-bold'>Question:</span> Why are you using firebase? What other options do you have to implement authentication? </h4>
