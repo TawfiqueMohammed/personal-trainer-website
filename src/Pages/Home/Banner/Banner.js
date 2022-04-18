c1928a653ddb23a376fcee140b1cbb2b7f931c6f
Tawfique Mohammed <tawfiquemt17@gmail.com>
2022-04-18 13:35:22 +0600

    carousel updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,44 +3,49 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../../images/banner/banner1.jpg'
 import banner2 from '../../../images/banner/banner2.jpg'
 import banner3 from '../../../images/banner/banner3.jpg'
+import './Banner.css'
 
 const Banner = () => {
     return (
         <div>
             <Carousel fade>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner1}
-                        alt="First slide"
-                    />
+                <Carousel.Item >
+                    <div className='item'>
+                        <img
+                            className="slide d-block w-100"
+                            src={banner3}
+                            alt="Second slide"
+                        />
+                    </div>
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3 className='text--white'>Holistic Personal Development</h3>
+                        <p>I am thrilled to share these holistic personal development courses with you.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner2}
-                        alt="Second slide"
-                    />
-
+                <Carousel.Item >
+                    <div className='item'>
+                        <img
+                            className="slide d-block w-100"
+                            src={banner1}
+                            alt="Second slide"
+                        />
+                    </div>
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3 className='text--white'> Self-Help Courses for You</h3>
+                        <p>Each course was created from the heart and designed around the skills I have found to be most powerful in my own life and in my work with clients.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner3}
-                        alt="Third slide"
-                    />
-
+                <Carousel.Item >
+                    <div className='item'>
+                        <img
+                            className="slide d-block w-100"
+                            src={banner2}
+                            alt="Second slide"
+                        />
+                    </div>
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3 className='text--white'>Professional Development for Mental Health Professionals</h3>
+                        <p>Each course is an investment in YOU and YOUR WELLNESS! Can’t wait to see you on the inside</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
